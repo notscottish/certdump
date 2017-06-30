30fbea85be86f23a6087328e953c7a5a48f9beaa
Scot Nottish <jerry@orcon.net.nz>
2017-06-30 16:20:13 +1200

    Added Subject Column

--- certdump.sh
+++ certdump.sh
@@ -17,13 +17,14 @@ fi
 
 exec 7<"$1"
 
+echo "\"Site:\",\"Certificate CN:\",\"Certificate SAN:\",\"Certificate Serial Number:\""
+
 while read -u 7; do
     if [ ${#REPLY} -eq 0 ]; then
         continue
     fi
     
     if ! echo "" | nc -G3 ${REPLY} 443 2>/dev/null; then
-        echo "info: port 443 not open: \"$REPLY\"" >&2
         echo "\"${REPLY}\""
         continue
     fi
@@ -34,10 +35,11 @@ while read -u 7; do
         continue
     fi
 
+	SUBJECT="$(openssl x509 -in ${REPLY}.crt -text 2>/dev/null | grep "Subject: " | sed -e "s/.*CN=//")"
     SAN="$(openssl x509 -in ${REPLY}.crt -text 2>/dev/null | grep "Subject Alternative Name:" -A1 | tail -1 | sed -e "s/DNS://g" )"
     SERIAL="$(openssl x509 -in ${REPLY}.crt -text 2>/dev/null | grep "Serial Number:" -A1 | tail -1 | sed -e "s/[\t ]//g")"
 
-    echo "\"${REPLY}\",\"${SAN}\",\"${SERIAL}\""
+    echo "\"${REPLY}\",\"${SUBJECT}\",\"${SAN}\",\"${SERIAL}\"" | sed -e 's/,"[\t ]*/,"/g'
 #    SAN="$(echo "" | openssl s_client -connect ${REPLY}:443 -servername ${REPLY} 2>/dev/null | openssl x509 -text | grep -A1 "Subject Alternative Name:" | tail -1)"
 #    SERIAL="$(echo "" | openssl s_client -connect ${REPLY}:443 -servername ${REPLY} 2>/dev/null | openssl x509 -text | grep -A1 "Serial Number:" | tail -1)"
 #    echo "\"${REPLY}\",\"${SAN}\",\"$SERIAL\"" | sed -e "s/ //g"
