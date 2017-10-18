ed1c42584677cfb01b3c2bbf6d998dd71f3deaa7
Scot Nottish <jerry@orcon.net.nz>
2017-10-18 20:59:35 +1300

    Now accepts FQDN as argument, rather than file with list of FQDNs.

--- certdump.sh
+++ certdump.sh
@@ -1,46 +1,29 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-    echo "usage: $(basename ${0}) fqdn_list_filename" >&2
+    echo "usage: $(basename ${0}) site_fqdn" >&2
     exit 0
 fi
 
-if [ ! -f "$1" ]; then
-    echo "error: file not found: \"$1\"" >&2
+if [ "$(echo $1 | grep -c "^\([a-zA-Z0-9-]\{1,\}\.\)\{1,\}[a-zA-Z0-9-]\{1,\}$")" -ne 1 ]; then
+   echo "usage: $(basename ${0}) site_fqdn" >&2
+   exit 1
+fi
+
+# echo "\"Certificate CN:\",\"Certificate SAN:\",\"Certificate Serial Number:\""
+if ! echo "" | nc -G3 ${1} 443 2>/dev/null; then
     exit 1
 fi
 
-# if ! TMPDIR="$(mktemp -d XXXXXX 2>/dev/null)"; then
-#     echo "error: temp folder creation failed: \"$TMPDIR\"" >&2
-#     exit 1
-# fi
+# Make sure to user the "-servername" option in case SNI is in place.
+if ! echo "" | openssl s_client -connect ${1}:443 -servername ${1} > ${1}.crt 2>/dev/null; then
+    # echo "warning: could not get cert for \"${1}\"" >&2
+    exit 1
+fi
 
-exec 7<"$1"
+SUBJECT="$(openssl x509 -in ${1}.crt -text 2>/dev/null | grep "Subject: " | sed -e "s/.*CN=//")"
+SAN="$(openssl x509 -in ${1}.crt -text 2>/dev/null | grep "Subject Alternative Name:" -A1 | tail -1 | sed -e "s/DNS://g" )"
+SERIAL="$(openssl x509 -in ${1}.crt -text 2>/dev/null | grep "Serial Number:" -A1 | tail -1 | sed -e "s/[\t ]//g")"
 
-echo "\"Site:\",\"Certificate CN:\",\"Certificate SAN:\",\"Certificate Serial Number:\""
-
-while read -u 7; do
-    if [ ${#REPLY} -eq 0 ]; then
-        continue
-    fi
-    
-    if ! echo "" | nc -G3 ${REPLY} 443 2>/dev/null; then
-        echo "\"${REPLY}\""
-        continue
-    fi
-
-    if ! echo "" | openssl s_client -connect ${REPLY}:443 -servername ${REPLY} > ${REPLY}.crt 2>/dev/null; then
-        # echo "warning: could not get cert for \"${REPLY}\"" >&2
-        echo "\"${REPLY}\""
-        continue
-    fi
-
-	SUBJECT="$(openssl x509 -in ${REPLY}.crt -text 2>/dev/null | grep "Subject: " | sed -e "s/.*CN=//")"
-    SAN="$(openssl x509 -in ${REPLY}.crt -text 2>/dev/null | grep "Subject Alternative Name:" -A1 | tail -1 | sed -e "s/DNS://g" )"
-    SERIAL="$(openssl x509 -in ${REPLY}.crt -text 2>/dev/null | grep "Serial Number:" -A1 | tail -1 | sed -e "s/[\t ]//g")"
-
-    echo "\"${REPLY}\",\"${SUBJECT}\",\"${SAN}\",\"${SERIAL}\"" | sed -e 's/,"[\t ]*/,"/g'
-#    SAN="$(echo "" | openssl s_client -connect ${REPLY}:443 -servername ${REPLY} 2>/dev/null | openssl x509 -text | grep -A1 "Subject Alternative Name:" | tail -1)"
-#    SERIAL="$(echo "" | openssl s_client -connect ${REPLY}:443 -servername ${REPLY} 2>/dev/null | openssl x509 -text | grep -A1 "Serial Number:" | tail -1)"
-#    echo "\"${REPLY}\",\"${SAN}\",\"$SERIAL\"" | sed -e "s/ //g"
-done
+# Output as quote encapsulated comma separated values (CSV)
+echo "\"${SUBJECT}\",\"${SAN}\",\"${SERIAL}\"" | sed -e 's/,"[\t ]*/,"/g'
